643af9a9e9d462e5ce68ede08f3d99af0f713bd1
rob noort <rcmnoort@gmail.com>
2019-11-07 07:51:47 +0100

    Trying out exceptions

--- src/main/java/nl/robnoort/App.java
+++ src/main/java/nl/robnoort/App.java
@@ -1,13 +1,53 @@
 package nl.robnoort;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        App app = new App();
+        
+        try {
+            app.testExceptions();
+        } catch (CannotSwimException e) {
+            e.printStackTrace();
+            throw new RuntimeException("hier");
+        }
+
     }
+
+    public void testExceptions() throws CannotSwimException {
+        try {
+            throw new CannotSwimException(" kan niet zwemmen ");
+           
+        } catch (Exception ex) {
+             ex.printStackTrace();
+            throw new RuntimeException(ex.getMessage());
+        }
+
+    }
+
+    class CannotSwimException  extends Exception {
+        
+        public CannotSwimException(String message) {
+            super(message);
+        }
+
+        private static final long serialVersionUID = 1L;
+    }
+
+    class DangerInTheWater extends RuntimeException {
+        private static final long serialVersionUID = 1L;
+    }
+
+    class SharkInTheWaterException extends DangerInTheWater {
+        private static final long serialVersionUID = 1L;
+    }
+
+
 }
